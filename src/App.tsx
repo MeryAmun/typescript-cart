fa5803580d6fe37f729c313aa08ee4f28efd5dad
MeryAmun <chebesiawah@gmail.com>
2021-12-03 01:05:43 +0100

    installed dependencies like material ui, reat query, styled components etc

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+import React, { useState } from 'react'
+
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
+import Badge from '@mui/material/Badge'
+import Drawer from '@mui/material/Drawer'
+import Grid from '@mui/material/Grid'
+import LinearProgress from '@mui/material/LinearProgress'
+import { Wrapper } from './App.styles'
+import { useQuery } from 'react-query'
+
+//styels
+
+function App() {
+  return <div className='App'>cart</div>
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import './index.css'
+
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const client = new QueryClient()
+
+ReactDOM.render(
+  <QueryClientProvider client={client}>
+    <App />
+  </QueryClientProvider>,
+  document.getElementById('root')
+)
